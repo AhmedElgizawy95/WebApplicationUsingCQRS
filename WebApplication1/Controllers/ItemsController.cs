b39bfcf7dba42dbd4a284f415ffdfc96d0242585
Ahmed Alaa <elgizawy.ahmed.21@gmail.com>
2024-07-06 23:56:51 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using CQRS_lib.Data.Models;
+using CQRS_lib.Repos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepo _repo;
+        public ItemsController(IItemsRepo repo) 
+        {
+            _repo = repo;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetItems()
+        {
+            return Ok(_repo.GetItems());
+        }
+
+        [HttpPost]   
+        public async Task<IActionResult> InsertItem(Items item)
+        {
+            _repo.InsertItem(item);
+            return Ok(item);
+
+        }
+    }
+}
